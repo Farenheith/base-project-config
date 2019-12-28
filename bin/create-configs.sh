f59fab76eae90f3ec110fe6c2d3b90d99a7fe751
Thiago Oliveira Santos <thiago.santos@maxmilhas.com.br>
2019-12-28 17:45:28 -0300

    test

--- bin/create-configs.sh
+++ bin/create-configs.sh
@@ -1,7 +1,9 @@
-{
-    "extends": "base-project-config/tslint.json"
-} > ./tslint.json
-mkdir test
-{
-    "extends": "base-project-config/tslint.test.json"
-} > ./test/tslint.json
+printf "{
+    \"extends\": \"base-project-config/tslint.json\"
+}
+" > ./tslint.json
+mkdir -p test
+printf "{
+    \"extends\": \"base-project-config/tslint.test.json\"
+}
+" > ./test/tslint.json
