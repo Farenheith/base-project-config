05ae994e8e2b7ea6b246d1a77353580223c1ed0f
Thiago Oliveira Santos <thiago.santos@maxmilhas.com.br>
2019-12-28 17:39:03 -0300

    test

---
+++ bin/create-configs.sh
@@ -0,0 +1,6 @@
+{
+    "extends": "base-project-config/tslint.json"
+} > ./tslint.json
+{
+    "extends": "base-project-config/tslint.test.json"
+} > ./test/tslint.json
---
+++ bin/setup-project.sh
@@ -0,0 +1,2 @@
+create-configs
+add-scripts
