b3927569c52a7dde1241d0d98ba356c47ac79ddd
Thiago Oliveira Santos <thiago.santos@maxmilhas.com.br>
2019-12-28 18:31:02 -0300

    test

--- bin/run-test.sh
+++ bin/run-test.sh
@@ -1,3 +1,7 @@
-[ -f ./test/.mocha.opts ] && \
-    mocha $1 --recursive ./test/**/*.spec.ts || \
+if [ -f ./test/.mocha.opts ]; then
+    echo "mocha $1 --recursive ./test/**/*.spec.ts"
+    mocha $1 --recursive ./test/**/*.spec.ts
+else
+    echo "mocha --opts ./node_modules/base-project-config/mocha.opts $1 --recursive ./test/**/*.spec.ts"
     mocha --opts ./node_modules/base-project-config/mocha.opts $1 --recursive ./test/**/*.spec.ts
+fi
