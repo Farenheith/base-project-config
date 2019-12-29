b2b16da1f20215c2276f20655680d4e989117b11
Thiago Oliveira Santos <thiago.santos@maxmilhas.com.br>
2019-12-29 05:14:50 -0300

    fixing tesconfig.test.json detect

--- ts-node-register.js
+++ ts-node-register.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
-const project = fs.existsSync('$PWD/tsconfig.ts.json') ?
-	'$PWD/tsconfig.ts.json' :
+const baseDir = __dirname.substring(0, __dirname.lastIndexOf('/node_modules/'));
+const tsConfig = `${baseDir}/tsconfig.test.json`;
+const project = fs.existsSync(tsConfig) ?
+	tsConfig :
 	'./node_modules/base-project-config/tsconfig.test.json';
 require('ts-node').register({ project });
