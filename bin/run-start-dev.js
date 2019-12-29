f3fffe8bee6c54a616b3ecbf64e16cda78531b6e
Thiago Oliveira Santos <thiago.santos@maxmilhas.com.br>
2019-12-29 00:27:47 -0300

    setting up debug command

--- bin/run-start-dev.js
+++ bin/run-start-dev.js
@@ -3,17 +3,20 @@
 'use strict';
 
 const fs = require('fs');
+let spliceIndex = 2;
 const baseDir = __dirname.substring(0, __dirname.lastIndexOf('/node_modules/'));
+console.log(baseDir);
 if (!fs.existsSync(`${baseDir}/node_modules/test/.mocha.opts`)) {
-    process.argv.push('--opts', './node_modules/base-project-config/mocha.opts');
+    process.argv.splice(spliceIndex, 0, '--opts', './node_modules/base-project-config/mocha.opts');
+    spliceIndex += 2;
 }
 const mochaSetup = `${baseDir}/test`;
 const setups = fs.readdirSync(mochaSetup)
 for (const setup of setups) {
     if (setup.match(/.*setup\.spec\.ts/)) {
-        process.argv.push(`test/${setup}`);
+        process.argv.splice(spliceIndex, 0, `test/${setup}`);
+        spliceIndex++
     }
 }
 process.argv.push('--recursive', './test/**/*.spec.ts');
-console.log(`exec mocha w/ args:`, process.argv.slice(2));
 require(`${baseDir}/node_modules/mocha/lib/cli`).main();
