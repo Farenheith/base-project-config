9d6f25d6cddd2c97cb652da7602206d623ee5253
Thiago Oliveira Santos <tos.oliveira@gmail.com>
2020-01-02 19:00:56 -0300

    adding prebuild script

--- bin/add-scripts.sh
+++ bin/add-scripts.sh
@@ -1,6 +1,6 @@
 npx npm-add-script -k "lint" -v "run-lint" -f
+npx npm-add-script -k "prebuild" -v "run-pre-build" -f
 npx npm-add-script -k "build" -v "run-build" -f
-npx npm-add-script -k "build:dev" -v "run-build-dev" -f
 npx npm-add-script -k "test:coverage" -v "run-test-coverage" -f
 npx npm-add-script -k "test:coverage:lcovonly" -v "run-test-lcov" -f
 npx npm-add-script -k "test:coverage:json" -v "run-test-json" -f
