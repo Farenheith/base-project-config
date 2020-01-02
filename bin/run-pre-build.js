97db22c04b301580b52905cd1bb5c34252248afe
Thiago Oliveira Santos <tos.oliveira@gmail.com>
2020-01-02 20:28:33 -0300

    fixing gulp script

--- bin/run-pre-build.js
+++ bin/run-pre-build.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 const baseDir = require('./get-base-dir');
-process.argv.push('-p', `${baseDir}/node_modules/base-project-config/gulpfile-clear.js`);
+process.argv.push('-f', `${baseDir}/node_modules/base-project-config/gulpfile-clear.js`);
 
 require(`${baseDir}/node_modules/gulp/bin/gulp`);
