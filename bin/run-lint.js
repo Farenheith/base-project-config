16b75c1439b53871029dbf1399bbcc6c94547e90
Thiago Oliveira Santos <tos.oliveira@gmail.com>
2020-01-02 20:36:39 -0300

    fixing lint script

--- bin/run-lint.js
+++ bin/run-lint.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 
 const baseDir = require('./get-base-dir');
-process.argv.push('-p', !fs.existsSync('./tsconfig.json') ? './tsconfig.json' :
+process.argv.push('-p', !fs.existsSync(`${baseDir}/tsconfig.json`) ? `${baseDir}/tsconfig.json` :
 	`${baseDir}/node_modules/base-project-config/tsconfig.json`);
 
 require(`${baseDir}/node_modules/tslint/bin/tslint`);
