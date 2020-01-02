ce4a1bcfdf5dc3230086d5d824aab1ee6d943bf1
Thiago Oliveira Santos <tos.oliveira@gmail.com>
2020-01-02 20:15:25 -0300

    forcing scripts

--- bin/add-scripts.js
+++ bin/add-scripts.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 var npmAddScript = require('npm-add-script');
 
-npmAddScript({ key: 'lint', value: 'run-lint'})
-npmAddScript({ key: 'prebuild', value: 'run-pre-build'})
-npmAddScript({ key: 'build', value: 'run-build'})
-npmAddScript({ key: 'test:coverage', value: 'run-test-coverage'})
-npmAddScript({ key: 'test:coverage:lcovonly', value: 'run-test-lcov'})
-npmAddScript({ key: 'test:coverage:json', value: 'run-test-json'})
-npmAddScript({ key: 'test', value: 'run-test'})
+npmAddScript({ key: 'lint', value: 'run-lint', force: true })
+npmAddScript({ key: 'prebuild', value: 'run-pre-build', force: true })
+npmAddScript({ key: 'build', value: 'run-build', force: true })
+npmAddScript({ key: 'test:coverage', value: 'run-test-coverage', force: true })
+npmAddScript({ key: 'test:coverage:lcovonly', value: 'run-test-lcov', force: true })
+npmAddScript({ key: 'test:coverage:json', value: 'run-test-json', force: true })
+npmAddScript({ key: 'test', value: 'run-test', force: true })
