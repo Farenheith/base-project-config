34fa424a51d85719d6f1566868103c6eeb224d7a
Thiago Oliveira Santos <tos.oliveira@gmail.com>
2020-01-02 21:56:18 -0300

    fixing codecov and travis

--- bin/run-test-json.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-const baseDir = require('./get-base-dir');
-process.argv.push('--reporter=json');
-require('./run-test-coverage');
--- gulpfile-coverage.js
+++
@@ -1,9 +0,0 @@
-const baseDir = require('./bin/get-base-dir');
-const gulp = require('gulp');
-const rename = require('gulp-rename');
-
-gulp.task('default', function () {
-	return gulp.src(`${baseDir}/coverage/coverage-final.json`)
-		.pipe(rename('coverage.json'))
-		.pipe(gulp.dest('.'));
-});
